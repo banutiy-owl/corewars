556433fcf04f908957868dd9a677050fb241c379
plaskonp <piotr.plaskonka.stud@pw.edu.pl>
2024-05-16 13:26:20 +0200

    dodanie class warrior i dodanie zapisywania oraz pobierania wojowników

---
+++ app/warrior.py
@@ -0,0 +1,25 @@
+class Warrior(object):
+
+    def __init__(self,name):
+        self.name = name,
+        self.won = 0,
+        self.lost = 0,
+        self.busy = False
+
+    
+    def __init__(self,name,won,lost,busy):
+        self.name = name,
+        self.won = won,
+        self.lost = lost,
+        self.busy = busy
+
+
+    def saveToDB(self,ref,user_id):
+        worrior_data = {
+            "user_id": user_id,
+            "name": self.name,
+            "won": self.won,
+            "lost": self.lost,
+            "busy": self.busy
+        }
+        ref.child('worriors').push(worrior_data)
