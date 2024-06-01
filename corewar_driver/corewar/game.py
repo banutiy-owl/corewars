55b5e13973898a76a8aa79279848a35ab8379409
PetrashU <petrashevichulyana@gmail.com>
2024-06-01 11:54:03 +0200

    Zrównoleglenie rozgrywki

--- corewar_driver/corewar/game.py
+++ corewar_driver/corewar/game.py
@@ -1,9 +1,25 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from corewar_driver.corewar import mars, redcode
+from corewar_driver.corewar.core import Core
 from corewar_driver.corewar.redcode import OPCODES
 
-def game(warrior_1_id, warrior_1_code, warrior_2_id, warrior_2_code, coresize = None, rounds = None, maxcycles = None):
-    core = coresize if coresize else 8000
-    DEFAULT_ENV = {'CORESIZE': core} #'MAXLENGTH': 100}
+def simulate_round(warrior_1, warrior_2, core_s, n_cycles):
+    core_cr = Core(size=core_s)
+    simulation = mars.MARS(warriors=[warrior_1, warrior_2], core=core_cr)
+    for x in range(n_cycles):
+        try:
+            simulation.step()
+            if not warrior_1.task_queue:
+                return x, warrior_2.id, simulation, None
+            elif not warrior_2.task_queue:
+                return x, warrior_1.id, simulation, None
+        except Exception as e:
+            return x, -1, simulation, e
+    return n_cycles, 0, simulation, None
+
+def game(warrior_1_id, warrior_1_code, warrior_2_id, warrior_2_code, coresize=None, rounds=None, maxcycles=None):
+    core_s = coresize if coresize else 8000
+    DEFAULT_ENV = {'CORESIZE': core_s}
     n_rounds = rounds if rounds else 10
     n_cycles = maxcycles if maxcycles else 8000
     
@@ -13,48 +29,31 @@ def game(warrior_1_id, warrior_1_code, warrior_2_id, warrior_2_code, coresize = 
     warrior_2.id = warrior_2_id
 
     cycles = n_rounds * [None]
-    wins = {}
-    wins[warrior_1_id] = 0
-    wins[warrior_2_id] = 0
+    wins = {warrior_1_id: 0, warrior_2_id: 0}
     round_winner_id = n_rounds * [None]
-    core_states = ""
+    core_states_l = [""] * n_rounds
     exceptions = n_rounds * [None]
 
-    for i in range(n_rounds):
-        # run simulation for at most
-        print(f"Round {i}")
-        simulation = mars.MARS(warriors=[warrior_1, warrior_2])
-        for x in range(n_cycles):
+    with ThreadPoolExecutor() as executor:
+        future_to_round = {executor.submit(simulate_round, warrior_1, warrior_2, core_s, n_cycles): i for i in range(n_rounds)}
+        
+        for future in as_completed(future_to_round):
+            i = future_to_round[future]
             try:
-                simulation.step()
-                if not warrior_1.task_queue: 
-                    cycles[i] = x
-                    wins[warrior_2_id] += 1
-                    round_winner_id[i] = warrior_2_id
-                    break
-                elif not warrior_2.task_queue:
-                    cycles[i] = x
-                    wins[warrior_1_id] += 1
-                    round_winner_id[i] = warrior_1_id
-                    break
+                cycle_count, winner_id, simulation, exception = future.result()
+                cycles[i] = cycle_count
+                round_winner_id[i] = winner_id
+                if winner_id > 0:
+                    wins[winner_id] += 1
+                exceptions[i] = exception
+                core_state = ""
+                for j in range(core_s):
+                    opcode = next(key for key, value in OPCODES.items() if value == simulation.core[j].opcode)
+                    core_state += f"{opcode},{simulation.core.get_owner(j)},{simulation.core.get_modified(j)};"
+                core_states_l[i] = core_state + "\n"
             except Exception as e:
-                cycles[i] = x
+                cycles[i] = -1
                 round_winner_id[i] = -1
-                exceptions[i] = e
-        else:
-            cycles[i] = n_cycles
-            round_winner_id[i] = 0
-        for i in range(core):
-            opcode = next(key for key,value in OPCODES.items() if value==simulation.core[i].opcode)
-            core_states = core_states + str(opcode) + ", " + str(simulation.core.get_owner(i)) + ", "+ str(simulation.core.get_modified(i)) + "; "
-        core_states = core_states+"\n"
-            
-    """cycles - lista cykli dla każdej rundy, 
-    round_winner_id - lista z id zwycięzcy każdej rundy
-        (0 jeżeli remis, 
-        -1 jeżely był error, wtedy tekst error jest pod odpowiednim indeksem w exceptions), 
-    wins - dict wygranych każdego wojownika,
-    core_states - (string) stany końcowe pól w formacie np. DAT, 123, 321;...
-        pierwsza liczba to owner, druga to modifier(warrior który ostatni zmienił komórkę)
-    exceptions - lista errorów, jęzeli zaszły"""
-    return(cycles, round_winner_id, wins, core_states, exceptions)
+                exceptions[i] = e + "This is a program error, please contact our technical support."
+    core_states = "".join(core_states_l)
+    return cycles, round_winner_id, wins, core_states, exceptions
