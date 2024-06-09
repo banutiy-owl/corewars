6b5305589d68c122e715f74d3cfe6c41c895b246
PetrashU <petrashevichulyana@gmail.com>
2024-06-09 20:48:11 +0200

    Wszystkie linki widoczne, pokazana obecna strona

--- frontend_project_corewars/src/components/screens/ListWarriorsPage/Header.js
+++ frontend_project_corewars/src/components/screens/ListWarriorsPage/Header.js
@@ -8,12 +8,17 @@ const Header = () => {
     navigate("/home");
   };
 
+
+  const handleHistoryClick = () => {
+    navigate("/history");
+  };
+
   const handleNewGame = () => {
     navigate("/new-game");
   };
 
-  const handleHistory = () => {
-    navigate("/history");
+  const handleWarriorsClick = () => {
+    navigate("/warriors");
   };
 
   return (
@@ -24,10 +29,13 @@ const Header = () => {
         </button>
       </div>
       <div className="right">
+        <button className="header-btn-current" onClick={handleWarriorsClick}>
+          Warriors
+        </button>
         <button className="header-btn" onClick={handleNewGame}>
           New Game
         </button>
-        <button className="header-btn" onClick={handleHistory}>History</button>
+        <button className="header-btn" onClick={handleHistoryClick}>History</button>
       </div>
     </header>
   );
