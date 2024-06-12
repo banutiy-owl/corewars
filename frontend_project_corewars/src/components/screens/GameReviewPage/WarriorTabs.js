125409e302535257496cb6bbdd1eac26f2361fe6
Kate Kononova <kattrusik@gmail.com>
2024-06-12 17:13:09 +0200

    Ikonki oraz kolory na game review

--- frontend_project_corewars/src/components/screens/GameReviewPage/WarriorTabs.js
+++ frontend_project_corewars/src/components/screens/GameReviewPage/WarriorTabs.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function WarriorTabs({ currentWarrior, setCurrentWarrior }) {
   const warriorCode = {
-    "Warrior 1": "Code for Warrior 1...",
-    "Warrior 2": "Code for Warrior 2...",
+    "Warrior 1": "Code for Warrior 1...", // tutaj kod
+    "Warrior 2": "Code for Warrior 2...", //tutaj kod
+  };
+
+
+  const getButtonStyle = (warrior) => {
+    if (currentWarrior === warrior) {
+      return warrior === "Warrior 1" ? { backgroundColor: "#f03838" } : { backgroundColor: "#4d27c9" };
+    }
+    return {};
   };
 
   return (
@@ -14,6 +22,7 @@ function WarriorTabs({ currentWarrior, setCurrentWarrior }) {
             key={warrior}
             className={currentWarrior === warrior ? "active" : ""}
             onClick={() => setCurrentWarrior(warrior)}
+            style={getButtonStyle(warrior)}
           >
             {warrior}
           </button>
